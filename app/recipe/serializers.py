7d0c0f24b6250e9b4b65b195f24f7ca3d3e5a839
joacotomasini <reeninja1996@gmail.com>
2021-04-28 18:21:26 -0300

    Ingredient endpoints created

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from core.models import Tag
+from core.models import Tag, Ingredient
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -8,8 +8,18 @@ class TagSerializer(serializers.ModelSerializer):
     Serializer for Tag objects
     """
 
-
     class Meta:
         model = Tag
+        fields = ('id', 'name')
+        read_only_fields = ('id',)
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+    """
+    Serializer for Ingredient objects
+    """
+
+    class Meta:
+        model = Ingredient
         fields = ('id', 'name')
         read_only_fields = ('id',)
