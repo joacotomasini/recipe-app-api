4d5600379943a782ca9c4696fd22df58a9fbcf48
joacotomasini <reeninja1996@gmail.com>
2021-04-30 19:40:48 -0300

    Recipe endpoints done

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -59,3 +59,18 @@ class RecipeViewSet(viewsets.ModelViewSet):
         Retrieve the recipes for the authenticated user
         """
         return self.queryset.filter(user=self.request.user)
+
+    def get_serializer_class(self):
+        """
+        Return the appropiate serializer
+        """
+        if self.action == 'retrieve':
+            return serializers.RecipeDetailSerializer
+
+        return self.serializer_class
+
+    def perform_create(self, serializer):
+        """
+        Create a new recipe
+        """
+        serializer.save(user=self.request.user)
